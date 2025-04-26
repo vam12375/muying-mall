bb8b084d3790200155579d6dd925258190953297
faction168 <2898191344@qq.com>
2025-04-26 14:18:12 +0800

    初始化

---
+++ src/main/java/com/muyingmall/MuyingMallApplication.java
@@ -0,0 +1,13 @@
+package com.muyingmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuyingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuyingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muyingmall/MuyingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muyingmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuyingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
